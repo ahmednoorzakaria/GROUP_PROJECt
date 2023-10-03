b93863fc36f67666323a0833239e169137295ead
emmanuel <emmanuel.mukasa@moringaschool.com>
2023-10-03 14:17:39 +0300

    added css stylings

--- Client/client/src/Components/LoginForm.jsx
+++ Client/client/src/Components/LoginForm.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { FaGoogle, FaFacebook, FaLinkedin, FaInstagram } from 'react-icons/fa';
-
+import './App.css';
 
 function LoginForm() {
   const initialValues = {
